21b67dcea760fb3710b99479bab8c696ffc9b723
KingJayan <jayanp0202@gmail.com>
2026-02-07 22:09:18 -0600

    refactor + start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/dt_teleop.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+@TeleOp(name = "DT TeleOp")
+public class dt_teleop extends OpMode {
+
+    private DcMotorEx leftFront, leftBack, rightFront, rightBack;
+
+
+    @Override public void init() {
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override public void loop() {
+        drive();
+    }
+
+    private void drive() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        double lf = y + x + rx;
+        double lb = y - x + rx;
+        double rf = y - x - rx;
+        double rb = y + x - rx;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1.0);
+
+        leftFront.setPower((lf / denominator));
+        leftBack.setPower((lb / denominator));
+        rightFront.setPower((rf / denominator));
+        rightBack.setPower((rb / denominator));
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/atlatl_26/new_code_here/New_Code.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.atlatl_26.new_code_here;
-
-public class New_Code {
-    //over
-}
